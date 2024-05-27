f5b418254742ccbd964e49ae357e6c69ad9e9291
Sibisi Mathebe <sibisirobert12@gmail.com>
2024-05-28 00:12:14 +0200

    Initialized frontend

---
+++ frontend/app/(home)/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
+export default function HomeLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (<>{children}</>);
+}
---
+++ frontend/app/layout.tsx
@@ -0,0 +1,17 @@
+import { Providers } from "./providers";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
