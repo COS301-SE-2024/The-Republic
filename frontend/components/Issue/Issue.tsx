3533d1182153423d94fd9b5f84de81fc063854ec
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-05-31 19:48:31 +0200

    Improved layout of Issue component

--- frontend/components/Issue/Issue.tsx
+++ frontend/components/Issue/Issue.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardFooter, CardHeader } from '../ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
 import { MessageCircle } from 'lucide-react';
+import MoreMenu from '../MoreMenu/MoreMenu';
 
 
 
@@ -21,27 +22,41 @@ interface IssueProps {
 }
 
 const Issue: React.FC<IssueProps> = ({ issue }) => {
+  const menuItems = ['Edit', 'Delete', 'Report'];
+  const isOwner = true; // will have to get this from api
+
+  const handleDelete = () => {
+    // call delete tweet endpoint here
+    console.log('Deleting issue:', issue.id);
+  };
+
   return (
     <Card className="mb-4">
       <CardHeader className='place-content-stretch'>
-        <div className="flex items-center ">
-          <div className="pr-2">
-            <Avatar >
-              <AvatarImage src="https://homecoming.messiah.edu/wp-content/uploads/2015/04/speaker-3-v2.jpg" />
-              <AvatarFallback>JD</AvatarFallback>
-            </Avatar>
+        <div className="flex items-center justify-between w-full">
+          <div className="flex items-center">
+            <div className="pr-2">
+              <Avatar>
+                <AvatarImage src="https://homecoming.messiah.edu/wp-content/uploads/2015/04/speaker-3-v2.jpg" />
+                <AvatarFallback>JD</AvatarFallback>
+              </Avatar>
+            </div>
+            <div>
+              <div className="flex items-center">
+                <div className="font-bold">{issue.author}</div>
+                <div className="mx-1 text-sm text-gray-500">·</div>
+                <div className="text-sm text-gray-500">{issue.timestamp}</div>
+              </div>
+              <div className="text-sm text-gray-600">{issue.username}</div>
+            </div>
           </div>
-          <div className="font-bold">{issue.author}</div>
-          <div className="ml-2 text-sm text-gray-600">{issue.username}</div>
-          
+          <MoreMenu menuItems={menuItems} isOwner={isOwner} onDelete={handleDelete} />
         </div>
-        <div className="text-sm text-gray-500">{issue.timestamp}</div>
         <div className="flex space-x-2 pt-2">
           <Badge variant="outline" className=''>{issue.category}</Badge>
           <Badge variant="outline" className=''>{issue.sentiment}</Badge>
-        </div>     
+        </div>
       </CardHeader>
-      
       <CardContent>
         
 
