0fb2913fb7b6dbd1973e5806ed5006b4ce99430d
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-06-01 11:24:57 +0200

    Added service role key var

--- backend/src/supabaseClient.ts
+++ backend/src/supabaseClient.ts
@@ -4,8 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string;
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
 
 export default supabase;
