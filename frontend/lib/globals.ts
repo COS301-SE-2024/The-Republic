de4cc4f8146caa41a861e39b9490e3b24a74d610
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-06-02 18:23:07 +0200

    Removed sensitive information

--- frontend/lib/globals.ts
+++ frontend/lib/globals.ts
@@ -2,7 +2,7 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = 'https://plvofqwscloxamqxcxhz.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsdm9mcXdzY2xveGFtcXhjeGh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY4MjI1NDAsImV4cCI6MjAzMjM5ODU0MH0.ftgWznPWGedNu8elAiZiQFqP6Az1zD6YjaJXcbm41e8';
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
