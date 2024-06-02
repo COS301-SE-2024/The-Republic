97e87611f8fb85a294797fa5755f78f1f6bce113
Sibisi Mathebe <sibisirobert12@gmail.com>
2024-06-02 08:17:34 +0200

    Moved home routes and layout to home group

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Sidebar from '@/components/Sidebar/Sidebar';
-import RightSidebar from '@/components/RightSidebar/RightSidebar';
-import Header from '@/components/Header/Header';
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -18,15 +15,8 @@ export default function RootLayout({
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
-          >
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <div className="flex flex-1">
-              <Sidebar />
-              <main className="flex-1 p-4">{children}</main>
-              <RightSidebar />
-            </div>
-          </div>
+        >
+          {children}
         </ThemeProvider>
       </body>
     </html>
