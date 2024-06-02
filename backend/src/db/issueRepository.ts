37495f87a1092e03f7188e228bce5c9deb192c50
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-06-02 19:48:19 +0200

    Updated getAllIssues query and getIssueById query to give more information

--- backend/src/db/issueRepository.ts
+++ backend/src/db/issueRepository.ts
@@ -1,18 +1,44 @@
 import Issue from "../models/issue";
-
 import supabase from "../services/supabaseClient";
 
 export default class IssueRepository {
-  async getAllIssues(): Promise<Issue[]> {
-    const { data, error } = await supabase.from("issue").select("*");
+  async getAllIssues(): Promise<any[]> {
+    const { data, error } = await supabase
+      .from("issue")
+      .select(`
+        *,
+        user: user_id (
+          user_id,
+          email_address,
+          username,
+          fullname,
+          image_url
+        ),
+        category: category_id (
+          name
+        )
+      `)
+      .order("created_at", { ascending: false });
     if (error) throw new Error(error.message);
-    return data as Issue[];
+    return data as any[];
   }
 
   async getIssueById(issueId: number): Promise<Issue | null> {
     const { data, error } = await supabase
       .from("issue")
-      .select("*")
+      .select(`
+        *,
+        user: user_id (
+          user_id,
+          email_address,
+          username,
+          fullname,
+          image_url
+        ),
+        category: category_id (
+          name
+        )
+      `)
       .eq("issue_id", issueId)
       .single();
     if (error) throw new Error(error.message);
