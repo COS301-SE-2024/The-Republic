c8de8d826af36f973867574c0805aaeae3c32d5d
Sibisi Mathebe <sibisirobert12@gmail.com>
2024-06-02 08:20:09 +0200

    Created login page

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,5 +1,33 @@
-import { notFound } from "next/navigation";
+"use client"
 
-export default function Login(): never {
-    notFound();
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+
+export default function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+
+    return (
+        <div className="flex w-max h-full mb-16 items-center">
+            <form className="flex flex-col gap-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input type="email" placeholder="Email" id="email"/>
+                <div className="flex flex-row mt-2">
+                    <Label htmlFor="password">Password</Label>
+                    <p 
+                        className="text-xs ml-3"
+                        onClick={() => setShowPassword(!showPassword)}
+                    >
+                        {showPassword ? "Hide" : "Show"}
+                    </p>
+                </div>
+                <Input 
+                    type={showPassword ? "text" : "password"} 
+                    placeholder="Password" id="password" />
+                <p className="mt-2 text-sm">Don&apos;t have an account? <a href="/singup">Signup</a></p> 
+                <Button type="submit" className="mt-4">Login</Button>
+            </form>
+        </div>
+    );
 }
