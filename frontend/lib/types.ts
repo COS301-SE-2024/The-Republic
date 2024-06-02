87c796835ca9a0ad5e62dcb80600da2604c9ce74
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-06-02 19:48:27 +0200

    Created types

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -1,1 +1,25 @@
 // Shared types
+
+export interface User {
+  user_id: string;
+  email_address: string;
+  username: string;
+  fullname: string;
+  image_url: string;
+}
+export interface Issue {
+    issue_id: number;
+    user_id: string;
+    username: string;
+    content: string;
+    created_at: string;
+    category: Category;
+    sentiment: string;
+    numberofcomments: number;
+    user: User;
+  }
+
+export interface Category {
+  id: number;
+  name: string;
+}
