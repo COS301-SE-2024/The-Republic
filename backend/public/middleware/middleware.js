76b70f4a494615c57c2153523da1509a9a047467
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-06-06 12:26:14 +0000

    Removed The Public Folder From The Backend

--- backend/public/middleware/middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serverMiddleare = void 0;
-const serverMiddleare = (req, res, next) => {
-  console.log("Middleware executed!");
-  next();
-};
-exports.serverMiddleare = serverMiddleare;
