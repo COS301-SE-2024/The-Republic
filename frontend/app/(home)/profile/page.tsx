3772674fbafb116e1560a4c3e8f9723937503989
ShamaKamina <u18073655@tuks.co.za>
2024-06-08 01:56:02 +0200

    Removed User issues and fixed the profile stats

--- frontend/app/(home)/profile/page.tsx
+++ frontend/app/(home)/profile/page.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ProfileHeader from "@/components/ProfileHeader/ProfileHeader";
 import ProfileStats from "@/components/ProfileStats/ProfileStats";
-import UserIssues from "@/components/UserIssues/UserIssues";
+
 
 function page() {
   return (
   <div>
     <ProfileHeader />
     <ProfileStats />
-    <UserIssues />
   </div>
   );
 }
--- frontend/components/ProfileStats/ProfileStats.tsx
+++ frontend/components/ProfileStats/ProfileStats.tsx
@@ -1,18 +1,40 @@
-// src/components/ProfileStats/ProfileStats.tsx
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { SquarePen } from "lucide-react";
 
 const ProfileStats: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState("issues");
+
+  const handleTabClick = (tab: string) => {
+    setSelectedTab(tab);
+  };
+
   return (
     <div className="flex space-x-4 px-4 py-1 border-b">
-      <div className="relative inline-block">
+      <div
+        className={`relative inline-block cursor-pointer ${
+          selectedTab === "issues" ? "text-green-500" : ""
+        }`}
+        onClick={() => handleTabClick("issues")}
+      >
         <span className="font-bold">42</span>{" "}
         <span className="text-gray-600">Issues</span>
-        <div className="absolute bottom-0 left-0 w-full h-1 bg-green-500"></div>
+        {selectedTab === "issues" && (
+          <div className="absolute bottom-0 left-0 w-full h-1 bg-green-500"></div>
+        )}
       </div>
-      <div className="relative inline-block">
+      <div
+        className={`relative inline-block cursor-pointer ${
+          selectedTab === "resolved" ? "text-green-500" : ""
+        }`}
+        onClick={() => handleTabClick("resolved")}
+      >
         <span className="font-bold">28</span>{" "}
         <span className="text-gray-600">Resolved</span>
+        {selectedTab === "resolved" && (
+          <div className="absolute bottom-0 left-0 w-full h-1 bg-green-500"></div>
+        )}
       </div>
       <div className="flex items-center ml-auto">
         <SquarePen className="w-4 h-4 mr-1" />
