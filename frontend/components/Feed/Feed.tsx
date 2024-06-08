58f150b467ca3d1e383ef3644f8fb3c386a490bb
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-06-08 13:43:34 +0200

    Removed mock avatar image and replaced it with image_url of each user

--- frontend/components/Feed/Feed.tsx
+++ frontend/components/Feed/Feed.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import Issue from "../Issue/Issue";
 import IssueInputBox from "@/components/IssueInputBox/IssueInputBox";
 import { Issue as IssueType } from "@/lib/types";
+import { useUser } from "@/lib/contexts/UserContext";
 
 const Feed = () => {
   const [issues, setIssues] = useState<IssueType[]>([]);
+  const { user } = useUser();
 
   useEffect(() => {
     const fetchIssues = async () => {
@@ -23,7 +25,7 @@ const Feed = () => {
 
   return (
     <div className="w-full px-6">
-      <IssueInputBox />
+      <IssueInputBox user={user} />
       {issues.map((issue) => (
         <Issue key={issue.issue_id} issue={issue} />
       ))}
