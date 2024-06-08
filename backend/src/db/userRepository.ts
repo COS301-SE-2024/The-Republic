e615b638db4cc5da09e6f08b6557730a715f7331
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-06-08 13:47:59 +0200

    Fixed linting issue

--- backend/src/db/userRepository.ts
+++ backend/src/db/userRepository.ts
@@ -1,13 +1,14 @@
 import supabase from "../services/supabaseClient";
+import { User } from "../models/issue";
 
 export default class UserRepository {
-  async getUserById(userId: string): Promise<any> {
+  async getUserById(userId: string): Promise<User> {
     const { data, error } = await supabase
       .from("user")
       .select("*")
       .eq("user_id", userId)
       .single();
     if (error) throw new Error(error.message);
-    return data;
+    return data as User;
   }
 }
