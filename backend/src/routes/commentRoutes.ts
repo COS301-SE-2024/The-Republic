655560a19815163e97e78a2c0b1a3de43bd58b3d
Sibisi Mathebe <sibisirobert12@gmail.com>
2024-06-11 16:27:12 +0200

    Updated comment router with user fetching middleware and add and delete endpoints

--- backend/src/routes/commentRoutes.ts
+++ backend/src/routes/commentRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import * as commentController from "../controllers/commentController";
+import { verifyAndGetUser } from "../middleware/middleware";
 
 const router = Router();
 
+router.use(verifyAndGetUser);
 router.get("/", commentController.getComments);
 router.get("/count", commentController.getNumComments);
+router.post("/add", commentController.addComment);
+router.delete("/delete", commentController.deleteComment);
 
 export default router;
