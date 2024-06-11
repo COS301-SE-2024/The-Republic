29ff6aebce74c6c93e89825938b564d3f443f080
Sibisi Mathebe <sibisirobert12@gmail.com>
2024-06-11 15:16:28 +0200

    Added middleware to verify JWT and get user ID

--- backend/src/middleware/middleware.ts
+++ backend/src/middleware/middleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import supabase from "../services/supabaseClient";
 
 export const serverMiddleare = (
   req: Request,
@@ -6,5 +7,37 @@ export const serverMiddleare = (
   next: NextFunction,
 ) => {
   console.log("Middleware executed!");
+  next();
+};
+
+export const verifyAndGetUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader === undefined) {
+    next();
+    return;
+  }
+
+  const { data, error } = await supabase.auth.getUser(
+    authHeader.split(" ")[1]
+  );
+
+  if (error) {
+    console.error(error);
+
+    if (error.status === 403) {
+      res.sendStatus(403);
+    } else {
+      res.sendStatus(500);
+    }
+
+    return;
+  }
+
+  req.body.user_id = data.user.id;
   next();
 };
