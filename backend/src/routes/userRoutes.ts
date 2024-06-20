2ddc0f06b39eb5ec382538ad6d6dd2febaecc59c
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-06-20 16:49:53 +0200

    Added route for editing profile

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { getUserById } from "../controllers/userController";
+import multer from "multer";
+import { getUserById, updateUserProfile } from "../controllers/userController";
+import { verifyAndGetUser } from "../middleware/middleware";
 
 const router = express.Router();
+const upload = multer();
 
-router.get("/:id", getUserById);
+router.get("/:id", verifyAndGetUser, getUserById);
+router.put("/:id", verifyAndGetUser, upload.single('profile_picture'), updateUserProfile);
 
 export default router;
