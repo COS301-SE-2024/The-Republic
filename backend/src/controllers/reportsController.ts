54812419136fd912385650b34c0b93af171eaba9
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-06-21 07:01:12 +0000

    A Controller For the Reports

---
+++ backend/src/controllers/reportsController.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from "express";
+import { sendResponse } from "../utils/response";
+import { APIResponse } from "../types/response";
+import ReportsService from "../services/reportsService";
+
+const reportsService = new ReportsService();
+
+export const getAllIssuesGroupedByResolutionStatus = async (req: Request, res: Response) => {
+  try {
+    const response = await reportsService.getAllIssuesGroupedByResolutionStatus(req.body);
+    sendResponse(res, response);
+  } catch (error) {
+    sendResponse(res, error as APIResponse);
+  }
+};
+
+export const getIssueCountsGroupedByResolutionStatus = async (req: Request, res: Response) => {
+  try {
+    const response = await reportsService.getIssueCountsGroupedByResolutionStatus(req.body);
+    sendResponse(res, response);
+  } catch (error) {
+    sendResponse(res, error as APIResponse);
+  }
+};
+
+export const getIssueCountsGroupedByResolutionAndCategory = async (req: Request, res: Response) => {
+  try {
+    const response = await reportsService.getIssueCountsGroupedByResolutionAndCategory(req.body);
+    sendResponse(res, response);
+  } catch (error) {
+    sendResponse(res, error as APIResponse);
+  }
+};
+
+export const getIssuesGroupedByCreatedAt = async (req: Request, res: Response) => {
+  try {
+    const response = await reportsService.getIssuesGroupedByCreatedAt(req.body);
+    sendResponse(res, response);
+  } catch (error) {
+    sendResponse(res, error as APIResponse);
+  }
+};
+
+export const getIssuesGroupedByCategory = async (req: Request, res: Response) => {
+  try {
+    const response = await reportsService.getIssuesGroupedByCategory(req.body);
+    sendResponse(res, response);
+  } catch (error) {
+    sendResponse(res, error as APIResponse);
+  }
+};
+
+export const getIssuesCountGroupedByCategoryAndCreatedAt = async (req: Request, res: Response) => {
+  try {
+    const response = await reportsService.getIssuesCountGroupedByCategoryAndCreatedAt(req.body);
+    sendResponse(res, response);
+  } catch (error) {
+    sendResponse(res, error as APIResponse);
+  }
+};
