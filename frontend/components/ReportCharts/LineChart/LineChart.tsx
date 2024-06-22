c9afee0f884d278121e48e0f15e7903f0e1b5d0c
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-06-22 20:49:08 +0000

    Integrated Line Chart Component

--- frontend/components/ReportCharts/LineChart/LineChart.tsx
+++ frontend/components/ReportCharts/LineChart/LineChart.tsx
@@ -1,27 +1,85 @@
 'use client';
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as echarts from 'echarts';
+import { IssuesGroupedByDate } from "@/lib/reports";
+import { formatDate } from "@/lib/utils";
 
 function LineChart() {
+  const [dates, setDates] = useState<string[]>(['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']);
+  const [data, setData] = useState<number[]>([150, 230, 224, 218, 135, 147, 260]);
+  const [returndedData, setReturndedData] = useState<IssuesGroupedByDate>({});
+
+  useEffect(() => {
+    const fetchIssues = async () => {
+      try {
+        const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/reports/groupedCreatedAt`;
+        const response = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify({
+            from: 0,
+            amount: 99,
+          }),
+          headers: {
+            "content-type": "application/json"
+          }
+        });
+        const apiResponse = await response.json();
+
+        if (apiResponse.success && apiResponse.data) {
+          setReturndedData(apiResponse.data);
+        } else {
+          console.error("Error fetching issues:", apiResponse.error);
+        }
+      } catch (error) {
+        console.error("Error fetching issues:", error);
+      }
+    };
+
+    fetchIssues();
+  }, []);
+
+  useEffect(() => {
+    if (returndedData) {
+      const dateCounts: { [key: string]: number } = {};
+
+      Object.keys(returndedData).forEach(date => {
+        const formattedDate = formatDate(date);
+        if (!dateCounts[formattedDate]) {
+          dateCounts[formattedDate] = 0;
+        }
+
+        dateCounts[formattedDate] += returndedData[date].length;
+      });
+
+      const datesData = Object.keys(dateCounts).reverse();
+      const updateData = datesData.map(date => dateCounts[date]);
+  
+      setDates(datesData);
+      setData(updateData);
+    }
+  }, [returndedData]);  
+
   useEffect(() => {
     // ECharts Line Chart
-    const lineChart = echarts.init(document.querySelector("#lineChart") as HTMLElement);
-    lineChart.setOption({
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [{
-        data: [150, 230, 224, 218, 135, 147, 260],
-        type: 'line',
-        smooth: true
-      }]
-    });
-  }, []);
+    if (dates.length > 0 && data.length > 0) {
+      const lineChart = echarts.init(document.querySelector("#lineChart") as HTMLElement);
+      lineChart.setOption({
+        xAxis: {
+          type: 'category',
+          data: dates
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: data,
+          type: 'line',
+          smooth: true
+        }]
+      });
+    }
+  }, [data]);
 
   return (
     <div className="col-lg-6">
