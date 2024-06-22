66aefe16920dde0b41fff8b2407ef1d829fc5ad5
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-06-22 21:16:44 +0000

    Integrated Stacked Line Chart

--- frontend/components/ReportCharts/StackedLineChart/StackedLineChart.tsx
+++ frontend/components/ReportCharts/StackedLineChart/StackedLineChart.tsx
@@ -1,89 +1,104 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as echarts from 'echarts';
+import mockData from '@/data/stacked';
+import { formatMoreDate } from "@/lib/utils";
 
 function StackedLineChart() {
   const chartRef = useRef(null);
+  const [data, setData] = useState<{ [key: string]: { [key: string]: number } }>(mockData);
 
   useEffect(() => {
-    if (chartRef.current) {
-      const chart = echarts.init(chartRef.current);
-
-      const option = {
-        tooltip: {
-          trigger: 'axis'
-        },
-        legend: {
-          data: ['Sales', 'Revenue', 'Customers']
-        },
-        grid: {
-          left: '4%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: {
-          type: 'category',
-          boundaryGap: false,
-          data: [ "0", "2018-09-19", "2018-09-19", "2018-09-19", "2018-09-19", "2018-09-19", "2018-09-19", "2018-09-19"]
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [
-          {
-            name: 'Water Issues',
-            type: 'line',
-            smooth: true,
-            stack: 'total',
-            data: [22, 31, 40, 28, 51, 42, 82, 56],
-            areaStyle: {
-              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                offset: 0,
-                color: 'rgb(66, 84, 241)'
-              }, {
-                offset: 0.2,
-                color: 'rgb(66, 84, 241, 0.3)'
-              }])
-            }
-          },
-          {
-            name: 'Electricity issues',
-            type: 'line',
-            smooth: true,
-            stack: 'total',
-            data: [25, 11, 32, 45, 32, 34, 52, 41],
-            areaStyle: {
-              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                offset: 0,
-                color: 'rgb(46, 202, 106)'
-              }, {
-                offset: 0.2,
-                color: 'rgb(46, 202, 106, 0.3)'
-              }])
-            }
-          },
-          {
-            name: 'Health Issues',
-            type: 'line',
-            smooth: true,
-            stack: 'total',
-            data: [27, 15, 11, 32, 18, 9, 24, 11],
-            areaStyle: {
-              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                offset: 0,
-                color: 'rgb(255, 119, 29)'
-              }, {
-                offset: 0.2,
-                color: 'rgb(255, 119, 29, 0.3)'
-              }])
-            }
+    const fetchIssues = async () => {
+      try {
+        const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/reports/groupedCategoryAndCreatedAt`;
+        const response = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify({
+            from: 0,
+            amount: 99,
+          }),
+          headers: {
+            "content-type": "application/json"
           }
-        ]
-      };
+        });
+        const apiResponse = await response.json();
 
-      chart.setOption(option);
-    }
+        if (apiResponse.success && apiResponse.data) {
+          setData(apiResponse.data);
+        } else {
+          console.error("Error fetching issues:", apiResponse.error);
+        }
+      } catch (error) {
+        console.error("Error fetching issues:", error);
+      }
+    };
+
+    fetchIssues();
   }, []);
+  
+  useEffect(() => {
+    if (data && Object.keys(data).length !== 0) {
+      if (chartRef.current) {
+        const chart = echarts.init(chartRef.current);
+
+        const dates = Array.from(new Set(Object.values(data).flatMap(Object.keys))).sort();
+        const seriesData = Object.keys(data).map(category => ({
+          name: category,
+          type: 'line',
+          smooth: true,
+          stack: 'total',
+          data: dates.map(date => (data[category][date]) || 0),
+          areaStyle: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+              offset: 0,
+              color: getColorForCategory(category)
+            }, {
+              offset: 0.2,
+              color: getColorForCategory(category, 0.3)
+            }])
+          }
+        }));
+
+        const option = {
+          tooltip: {
+            trigger: 'axis'
+          },
+          legend: {
+            data: Object.keys(data)
+          },
+          grid: {
+            left: '4%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: formatMoreDate(dates)
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: seriesData
+        };
+
+        chart.setOption(option);
+      }
+    }
+  }, [data]);
+
+  const getColorForCategory = (category: string, opacity = 1) => {
+    const colors = {
+      'Public Safety': `rgba(255, 0, 0, ${opacity})`,
+      'Water': `rgba(0, 0, 255, ${opacity})`,
+      'Healthcare Services': `rgba(0, 255, 0, ${opacity})`,
+      'Electricity': `rgba(255, 255, 0, ${opacity})`,
+      'Administrative Services': `rgba(128, 128, 128, ${opacity})`
+    } as {[key: string]:  string };
+
+    return colors[category] || `rgba(0, 0, 0, ${opacity})`;
+  };
 
   return (
     <div className="col-12">
