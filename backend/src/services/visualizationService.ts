62e5af7dc6fb6a293a10e0c9e6e8725302e785ca
Sibisi Mathebe <sibisirobert12@gmail.com>
2024-06-23 12:18:22 +0200

    Created VisualizationService

---
+++ backend/src/services/visualizationService.ts
@@ -0,0 +1,18 @@
+import { VisualizationRepository } from "../db/visualizationRepository";
+import { APIData } from "../types/response";
+
+export class VisualizationService {
+  private visualizationRepository = new VisualizationRepository();
+
+  async getVizData() {
+    const vizData = await this.visualizationRepository.getVizData();
+
+    console.log(vizData);
+
+    return APIData({
+      code: 200,
+      success: true,
+      data: vizData
+    });
+  }
+}
