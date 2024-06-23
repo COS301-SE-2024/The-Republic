f165dce11c03935e147f5e91e11ab57a2dc09c45
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-06-23 01:48:40 +0000

    Changed The Colors For The Headings

--- frontend/app/(home)/visualizations/page.tsx
+++ frontend/app/(home)/visualizations/page.tsx
@@ -9,7 +9,7 @@ function page() {
   return (
     <section className="section align-center mb-5">
       <h3 className="mb-10 text-3xl font-extrabold leading-none tracking-tight text-gray-700 md:text-3xl lg:text-4xl dark:text-white text-center">
-        Discover <span className="text-blue-600 dark:text-blue-500">Issues</span> faced Across Provinces.
+        Discover <span className="text-green-600 dark:text-green-500">Issues</span> faced Across Provinces.
       </h3>
       <div>
         <EChartsComponent />
