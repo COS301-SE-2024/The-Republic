4d895693b81bd2f11fb3bbb06f9968549775daa1
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-08 23:58:04 +0200

    Updated the Server Instructions

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,4 +7,7 @@ const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`Press Ctrl+C to stop the server`);
+  console.log(`Visit http://localhost:${PORT} to test Backend API`);
+  console.log();
 });
