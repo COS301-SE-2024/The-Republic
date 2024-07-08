86094618c661b7c6f94d9c808303e3a47214d94a
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-09 00:55:13 +0200

    Removed Unused Jest Dom Type

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -37,7 +37,7 @@ module.exports = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/',  "<rootDir>/cypress/"],
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
     // Use babel-jest to transpile tests with the next/babel preset
     // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
