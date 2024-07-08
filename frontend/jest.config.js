1c197c499c891842b51438c0edf4edc78f40a5be
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-08 23:05:46 +0200

    (update) Jest Configuration File

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,24 +1,52 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|scss|sass)$': '<rootDir>/styleMock.ts',
-  },
   collectCoverage: true,
+  // on node 14.x coverage provider v8 offers good speed and more or less good report
+  coverageProvider: 'v8',
   collectCoverageFrom: [
-    '**/*.{ts,tsx}',
+    '**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
-    '!**/index.ts',
     '!**/node_modules/**',
-    '!**/.next/**',
-    '!coverage/**',
+    '!<rootDir>/out/**',
+    '!<rootDir>/.next/**',
+    '!<rootDir>/*.config.js',
+    '!<rootDir>/coverage/**',
   ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
+  moduleNameMapper: {
+    // Handle CSS imports (with CSS modules)
+    // https://jestjs.io/docs/webpack#mocking-css-modules
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+ 
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+ 
+    // Handle image imports
+    // https://jestjs.io/docs/webpack#handling-static-assets
+    '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i': `<rootDir>/__mocks__/fileMock.js`,
+ 
+    // Handle module aliases
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/(.*)$': '<rootDir>/$1',
+ 
+    // Handle @next/font
+    '@next/font/(.*)': `<rootDir>/__mocks__/nextFontMock.js`,
+    // Handle next/font
+    'next/font/(.*)': `<rootDir>/__mocks__/nextFontMock.js`,
+    // Disable server-only
+    'server-only': `<rootDir>/__mocks__/empty.js`,
+  },
+  // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/',  "<rootDir>/cypress/"],
+  testEnvironment: 'node',
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
+  preset: 'ts-jest',
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)']
 };
