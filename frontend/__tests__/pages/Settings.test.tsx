30813924c218c641e74c8283026884a29b6a766b
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-09 02:30:35 +0200

    (tests) Unit Tests for the Settings page

---
+++ frontend/__tests__/pages/Settings.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect } from '@jest/globals';
+import Home from '@/app/(home)/settings/page';
+
+jest.mock('@/components/Settings/Settings', () => jest.fn(() => <div>Mocked Settings</div>));
+
+describe('Settings Page', () => {
+  it('renders the Settings component', () => {
+    render(<Home />);
+    expect(screen.getByText('Mocked Settings')).not.toBeNull();
+  });
+});
