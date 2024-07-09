8e19919c0c202650ccf82cc57829a231ca49eb0c
Sibisi Mathebe <sibisirobert12@gmail.com>
2024-07-09 19:08:52 +0200

    Added middleware to redirect to login for pages that need authentication

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,11 +1,23 @@
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { supabase } from "./lib/globals";
 
-export function middleware(request: NextRequest) {
-  console.log(request);
+export async function middleware(request: NextRequest) {
+  const url = new URL(request.url);
+
+  const jwt = request.cookies.get("Authorization")?.value ?? '';
+  const { error } = await supabase.auth.getUser(jwt);
+
+  if (error) {
+    return NextResponse.redirect(new URL("/login", url.origin));
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: [],
+  matcher: [
+    "/notifications",
+    "/settings"
+  ]
 };
