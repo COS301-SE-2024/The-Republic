b3c8e1c29edd638dea8e01e3eb293b4bc5fa3991
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-10 10:05:55 +0200

    Fixed Config and Installed New Dependencies

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -14,10 +14,10 @@ module.exports = {
   moduleNameMapper: {
     // Handle CSS imports (with CSS modules)
     // https://jestjs.io/docs/webpack#mocking-css-modules
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^.+\\.module\\.(css|less|sass|scss)$': 'identity-obj-proxy',
  
     // Handle CSS imports (without CSS modules)
-    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '^.+\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
  
     // Handle image imports
     // https://jestjs.io/docs/webpack#handling-static-assets
