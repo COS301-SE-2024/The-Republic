2fd0a6d9c1cf24814f84e8b11ed3565eae17d87b
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-10 07:20:54 +0200

    Updated Depenencies and Jest Config

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,2 +1,10 @@
 // import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
+
+window.matchMedia = window.matchMedia || function() {
+    return {
+        matches: false,
+        addListener: function() {},
+        removeListener: function() {}
+    };
+};
