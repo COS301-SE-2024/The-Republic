b5406eef31832a65af1fd0e48dde257b227c8c44
ShamaKamina <u18073655@tuks.co.za>
2024-07-14 17:19:39 +0200

    Added the white theme logo for the sign up and login page

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -1,25 +1,39 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "@/styles/Custom.module.css";
+import { useTheme } from "next-themes";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [logoSrc, setLogoSrc] = useState("/images/b-logo-full-black-vert.png"); 
+  const { theme, systemTheme } = useTheme();
+
+  useEffect(() => {
+    const currentTheme = theme === "system" ? systemTheme : theme;
+    const newLogoSrc = currentTheme === "dark" 
+      ? "/images/b-logo-full-vert.png" 
+      : "/images/b-logo-full-black-vert.png";
+    setLogoSrc(newLogoSrc);
+  }, [theme, systemTheme]);
+
   return (
     <div className="flex flex-col w-dvw h-dvh items-center">
-      <div className="flex flex-col items-center mt-16  mb-2"> 
+      <div className="flex flex-col items-center mt-16 mb-2"> 
         <Image
           priority
           width={200} 
           height={200} 
-          src="/images/b-logo-full-vert.png"
+          src={logoSrc}
           alt="The Republic logo"
           className={styles.logo} 
         />
       </div>
-      <div > 
+      <div> 
         {children}
       </div>
     </div>
