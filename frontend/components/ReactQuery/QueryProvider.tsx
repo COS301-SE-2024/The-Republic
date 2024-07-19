244345541635a66280a0749b4cb52ab3d6320460
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-19 23:30:10 +0200

    Updated The React-Query Component

--- frontend/components/ReactQuery/QueryProvider.tsx
+++ frontend/components/ReactQuery/QueryProvider.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from '@/components/ReactQuery/QueryClient';
 
 const QueryProvider = ({ children }: { children: React.ReactNode }) => {
   return (
