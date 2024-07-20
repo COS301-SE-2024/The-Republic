2c5b3f507a6136e51e863f6ba7d8141de0e736f4
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-20 23:10:27 +0200

    Comments: Helper Methods For Making API Calls

---
+++ frontend/lib/api/deleteComment.tsx
@@ -0,0 +1,32 @@
+import { UserAlt as User } from "@/lib/types";
+
+const deleteComment = async (user: User, commentToDelete: string | null): Promise<void> => {
+    const headers: HeadersInit = {
+      "Content-Type": "application/json",
+    };
+  
+    if (user) {
+      headers.Authorization = `Bearer ${user.access_token}`;
+    }
+  
+    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/comments/delete`;
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers,
+      body: JSON.stringify({ comment_id: commentToDelete }),
+    });
+
+    if (response.status === 204) {
+      return;
+    }
+  
+    const apiResponse = await response.json();    
+  
+    if (apiResponse.ok) {
+      return apiResponse;
+    } else {
+      throw new Error(apiResponse.error || 'Failed to delete comment');
+    }
+};
+
+export { deleteComment };
