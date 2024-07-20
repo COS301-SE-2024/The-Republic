281f35b240c57ef6f277692613a01223be3ab1e3
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-20 23:10:52 +0200

    Issues: Helper Methods For Making API Calls

---
+++ frontend/lib/api/fetchIssues.tsx
@@ -0,0 +1,44 @@
+import { UserAlt as User, RequestBody } from "@/lib/types";
+
+const fetchIssues = async (user: User, sortBy: string, filter: string) => {
+  const headers: HeadersInit = {
+    "Content-Type": "application/json",
+  };
+
+  if (user) {
+    headers.Authorization = `Bearer ${user.access_token}`;
+  }
+
+  const requestBody: RequestBody = {
+    from: 0,
+    amount: 99,
+    order_by:
+      sortBy === "newest"
+        ? "created_at"
+        : sortBy === "oldest"
+        ? "created_at"
+        : "comment_count",
+    ascending: sortBy === "oldest",
+  };
+
+  if (filter !== "All") {
+    requestBody.category = filter;
+  }
+
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/issues`;
+  const response = await fetch(url, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(requestBody),
+  });
+
+  const apiResponse = await response.json();
+
+  if (apiResponse.success && apiResponse.data) {
+    return apiResponse.data;
+  } else {
+    throw new Error(apiResponse.error || 'Error fetching issues');
+  }
+};
+
+export { fetchIssues };
