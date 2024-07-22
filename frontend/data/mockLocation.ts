7d6877b9edb0e0d9cc66e659f723a162f73fdd46
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-22 06:01:25 +0200

    (update) Tests and Mock Location for RightSidebar

---
+++ frontend/data/mockLocation.ts
@@ -0,0 +1,11 @@
+import { Location } from "@/lib/types";
+
+const mockLocation: Location = {
+    location_id: "1",
+    province: "SomeProvince",
+    city: "SomeCity",
+    suburb: "SomeSuburb",
+    district: "SomeDistrict",
+};
+
+export default mockLocation;
