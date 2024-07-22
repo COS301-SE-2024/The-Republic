1f711f1a296377eea85b5fbbde84cc3deadabcb7
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-22 05:16:40 +0200

    Addwd New Mocks and Interfaces

---
+++ frontend/data/mockUser.ts
@@ -0,0 +1,16 @@
+import { MockUser } from "@/lib/types";
+
+const mockUser: MockUser = {
+    user_id: "user123",
+    email_address: "user@example.com",
+    username: "user123",
+    fullname: "User Fullname",
+    image_url: "http://example.com/image.jpg",
+    bio: "User biography",
+    is_owner: true,
+    total_issues: 10,
+    resolved_issues: 5,
+    access_token: "access_token_value",
+};
+
+export default mockUser;
--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -23,6 +23,19 @@ interface UserAlt {
   access_token: string;
 }
 
+interface MockUser {
+  user_id: string;
+  email_address: string;
+  username: string;
+  fullname: string;
+  image_url: string;
+  bio: string;
+  is_owner: boolean;
+  total_issues: number;
+  resolved_issues: number;
+  access_token: string;
+}
+
 interface UserContextType {
   user: UserAlt | null;
 }
@@ -243,6 +256,18 @@ interface ProfileFeedProps {
   selectedTab: "issues" | "resolved";
 }
 
+interface MulterFile {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  size: number;
+  destination: string;
+  filename: string;
+  path: string;
+  buffer: Buffer;
+}
+
 export type {
   AnalysisResult,
   FeedProps,
@@ -270,5 +295,7 @@ export type {
   ProfileUpdate,
   UserContextType,
   ProfileFeedProps,
-  ReactionProps
+  ReactionProps,
+  MockUser,
+  MulterFile
 };
