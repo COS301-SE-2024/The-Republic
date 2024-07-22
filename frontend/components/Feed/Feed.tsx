aa09227a6820c6e79fdb018e86305898a5f3c9d9
Sibisi Mathebe <sibisirobert12@gmail.com>
2024-07-22 13:51:08 +0200

    Extracted lazy list into a component

--- frontend/components/Feed/Feed.tsx
+++ frontend/components/Feed/Feed.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import Issue from "../Issue/Issue";
 import IssueInputBox from "@/components/IssueInputBox/IssueInputBox";
 import RightSidebar from "@/components/RightSidebar/RightSidebar";
@@ -9,142 +9,101 @@ import {
 import styles from './Feed.module.css';
 import { Loader2 } from "lucide-react";
 import { useUser } from "@/lib/contexts/UserContext";
+import { LazyList, LazyListRef } from "../LazyList/LazyList";
 
 const FETCH_SIZE = 2;
 
 const Feed: React.FC = () => {
   const { user } = useUser();
-  const [issues, setIssues] = useState<(IssueType | "Loading")[]>(["Loading"]);
   const [sortBy, setSortBy] = useState("newest");
   const [filter, setFilter] = useState("All");
+  const lazyRef = useRef<LazyListRef<IssueType>>(null);
 
-  useEffect(() => {
-    if (issues[issues.length - 1] === "Loading") {
-      const loadFrom = issues.length - FETCH_SIZE;
+  const fetchIssues = async (from: number, amount: number) => {
+    const headers: HeadersInit = {
+      "Content-Type": "application/json",
+    };
 
-      let ignoreResult = false;
-      const fetchIssues = async () => {
-        const headers: HeadersInit = {
-          "Content-Type": "application/json",
-        };
-
-        if (user) {
-          headers.Authorization = `Bearer ${user.access_token}`;
-        }
-
-        const requestBody: RequestBody = {
-          from: loadFrom <= 0 ? 0 : (loadFrom - 1) + FETCH_SIZE,
-          amount: FETCH_SIZE,
-          order_by: (() => {
-            switch (sortBy) {
-              case "newest":
-              case "oldest":
-                return "created_at";
-              default:
-                return "comment_count";
-            }
-          })(),
-          ascending: sortBy === "oldest",
-        };
-
-        if (filter !== "All") {
-          requestBody.category = filter;
-        }
-
-        const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/issues`;
-        const response = await fetch(url, {
-          method: "POST",
-          headers,
-          body: JSON.stringify(requestBody),
-        });
-
-        const apiResponse = await response.json();
-
-        if (ignoreResult) return;
-
-        if (apiResponse.success) {
-          const apiIssues = apiResponse.data as IssueType[];
-
-          issues.pop();
-          if (apiIssues.length > 0) {
-            setIssues([...issues, ...apiIssues, "Loading"]);
-          } else {
-            setIssues([...issues]);
-          }
-        }
-      };
-
-      let observer: IntersectionObserver | null = null;
-
-      if (loadFrom > 0) {
-        const handleIntersection = (
-          [entry]: IntersectionObserverEntry[],
-          observer: IntersectionObserver
-        )  => {
-          if (!entry.isIntersecting) return;
-
-          observer.unobserve(entry.target);
-          fetchIssues();
-        };
-
-        const loadFromElement = document.querySelector(`#issue_${loadFrom}`);
-        const rootElement = document.querySelector("#issues_scroll");
-
-        observer = new IntersectionObserver(handleIntersection, {
-          root: rootElement,
-        });
-
-        observer.observe(loadFromElement!);
-      } else {
-        fetchIssues();
-      }
-
-      return () => {
-        ignoreResult = true;
-        observer?.disconnect();
-      };
+    if (user) {
+      headers.Authorization = `Bearer ${user.access_token}`;
     }
-  });
 
-  const LoadingIndicator = ({ id }: { id: string}) => (
-    <div className="flex justify-center items-center h-32" id={id}>
+    const requestBody: RequestBody = {
+      from,
+      amount,
+      order_by: (() => {
+        switch (sortBy) {
+          case "newest":
+          case "oldest":
+            return "created_at";
+          default:
+            return "comment_count";
+        }
+      })(),
+      ascending: sortBy === "oldest",
+    };
+
+    if (filter !== "All") {
+      requestBody.category = filter;
+    }
+
+    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/issues`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(requestBody),
+    });
+
+    const apiResponse = await response.json();
+
+    if (apiResponse.success) {
+      return apiResponse.data as IssueType[];
+    } else {
+      // Should throw or return null here
+      return [];
+    }
+  };
+
+  const LoadingIndicator = () => (
+    <div className="flex justify-center items-center h-32">
       <Loader2 className="h-6 w-6 animate-spin text-green-400" />
     </div>
   );
 
+  const EmptyIndicator = () => (
+    <div className="flex justify-center items-center h-32">
+      <h3 className="text-lg">No issues</h3>
+    </div>
+  );
+
+  const scrollId = "issues_scroll";
+
   return (
     <div className="flex h-full">
       <div
-        className={`flex-1 overflow-y-auto px-6 ${styles['feed-scroll']}`}
-        id="issues_scroll"
+        className={`flex-1  px-6  overflow-y-scroll ${styles['feed-scroll']}`}
+        id={scrollId}
       >
         { user && <IssueInputBox user={user} />}
-        { issues.length > 0
-          ? issues.map((issue, index) => {
-            const id = `issue_${index + 1}`;
-
-            if (issue === "Loading") {
-              return <LoadingIndicator id={id}/>;
-            } else {
-              return <Issue issue={issue} id={id}/>;
-            }
-          })
-          : (
-            <div className="flex justify-center items-center h-32">
-              <h3 className="text-lg">No issues</h3>
-            </div>
-          )
-        }
+        <LazyList
+          pageSize={FETCH_SIZE}
+          fetcher={fetchIssues}
+          Item={({ data }) => <Issue issue={data}/>}
+          Loading={LoadingIndicator}
+          Empty={EmptyIndicator}
+          parentId={scrollId}
+          controlRef={lazyRef}
+        />
       </div>
       <RightSidebar
         sortBy={sortBy}
         setSortBy={(_sortyBy) => {
-          setIssues(["Loading"]);
+          lazyRef.current?.reload();
           setSortBy(_sortyBy);
         }}
         filter={filter}
         setFilter={(_filter) => {
-          setIssues(["Loading"]);
+          lazyRef.current?.reload();
           setFilter(_filter);
         }}
       />
---
+++ frontend/components/LazyList/LazyList.tsx
@@ -0,0 +1,152 @@
+import {
+  ReactNode,
+  Ref,
+  useEffect,
+  useImperativeHandle,
+  useState
+} from "react";
+import { v4 as uuidv4 } from "uuid";
+import equal from "fast-deep-equal";
+
+export interface LazyListRef<D> {
+  reload: () => void;
+  add: (data: D) => void;
+  remove: (data: D, compare?: (one: D, two: D) => boolean) => boolean;
+}
+
+interface LazyListProps<D> {
+  Item: (props: { data: D}) => ReactNode;
+  Loading: () => ReactNode;
+  Empty: () => ReactNode;
+  fetcher: (from: number, amount: number) => Promise<D[]>;
+  pageSize: number;
+  parentId?: string;
+  controlRef?: Ref<LazyListRef<D>>;
+  uniqueId?: string;
+}
+
+export function LazyList<D>({
+  Item,
+  Loading,
+  Empty,
+  fetcher,
+  pageSize,
+  parentId,
+  controlRef,
+  uniqueId = uuidv4()
+}: LazyListProps<D>) {
+  const [items, setItems] = useState<(D | "Loading" | "Deleted" )[]>(["Loading"]);
+
+  useImperativeHandle(controlRef, () => ({
+    reload: () => setItems(["Loading"]),
+
+    add: (data) => setItems([data, ...items]),
+
+    remove: (data, compare = equal) => {
+      const index = items.findIndex((item) =>
+        item !== "Loading" &&
+        item !== "Deleted" &&
+        compare(item, data)
+      );
+
+      if (index !== -1) {
+        items[index] = "Deleted";
+        setItems([...items]);
+
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }), [items]);
+
+  useEffect(() => {
+    if (items[items.length - 1] === "Loading") {
+      const loadFrom = items.length - pageSize;
+
+      let ignoreResult = false;
+      const getItems = async () => {
+        const fetchedItems = await fetcher(
+          loadFrom <= 0 ? 0 : (loadFrom - 1) + pageSize,
+          pageSize,
+        );
+
+        if (ignoreResult) return;
+
+        items.pop();
+        if (fetchedItems.length > 0) {
+          setItems([...items, ...fetchedItems, "Loading"]);
+        } else {
+          setItems([...items]);
+        }
+      };
+
+      let observer: IntersectionObserver | null = null;
+
+      if (loadFrom > 0) {
+        const handleIntersection = (
+          [entry]: IntersectionObserverEntry[],
+          observer: IntersectionObserver
+        )  => {
+          if (!entry.isIntersecting) return;
+
+          observer.unobserve(entry.target);
+          getItems();
+        };
+
+        const loadFromElement = document.querySelector(
+          `#item_${loadFrom}-${uniqueId}`
+        );
+        const rootElement = document.querySelector(
+          parentId ?? `#items_scroll-${uniqueId}`
+        );
+
+        observer = new IntersectionObserver(handleIntersection, {
+          root: rootElement,
+        });
+
+        observer.observe(loadFromElement!);
+      } else {
+        getItems();
+      }
+
+      return () => {
+        ignoreResult = true;
+        observer?.disconnect();
+      };
+    }
+  });
+
+  const isEmpty =
+    items.length === 0 ||
+    items.every((item) => item === "Deleted");
+
+  return (
+    <div
+      className={`flex flex-col h-full ${parentId || "overflow-y-scroll"}`}
+      id={`items_scroll-${uniqueId}`}
+    >
+      { !isEmpty
+        ? items.map((item, index) => {
+          const id = `item_${index + 1}-${uniqueId}`;
+
+          return (
+            <div id={id} key={id}>
+              {(() => {
+                switch(item) {
+                  case "Loading":
+                    return <Loading/>;
+                  case "Deleted":
+                    return <></>;
+                  default:
+                    return <Item data={item}/>;
+                }
+              })()}
+            </div>
+          );
+        })
+        : <Empty/>
+      }
+    </div>
+  );
+}
