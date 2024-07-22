79a68ebce101471fdcd39dc38dc8f225dbdbadfc
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-22 05:14:25 +0200

    (update) Backend Types After Code Change

---
+++ backend/src/types/data/mockFile.ts
@@ -0,0 +1,15 @@
+import { MulterFile } from "../users";
+
+const mockFile: MulterFile = {
+    fieldname: "image",
+    originalname: "profile.png",
+    encoding: "7bit",
+    mimetype: "image/png",
+    size: 1024,
+    destination: "uploads/",
+    filename: "profile.png",
+    path: "uploads/profile.png",
+    buffer: Buffer.from(""),
+};
+
+export default mockFile;
