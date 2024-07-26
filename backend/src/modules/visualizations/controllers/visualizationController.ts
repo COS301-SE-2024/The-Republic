414b55461409fef2e30b2c3a5f77a2f44a7c7dc7
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-26 20:22:40 +0200

    (Visualizations) Moved The Modules To Separate Dir

---
+++ backend/src/modules/visualizations/controllers/visualizationController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { VisualizationService } from "@/modules/visualizations/services/visualizationService";
+import { APIResponse } from "@/types/response";
+import { sendResponse } from "@/utils/response";
+
+const visualizationService = new VisualizationService();
+
+export async function getVizData(req: Request, res: Response) {
+  try {
+    const response = await visualizationService.getVizData();
+    sendResponse(res, response);
+  } catch (error) {
+    sendResponse(res, error as APIResponse);
+  }
+}
