7fb8474f115fad04e1cd10e3e6b1aac13444d873
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-26 16:49:11 +0200

    Moved Visualizations and Reports Pages To Components

--- frontend/components/ReportCharts/Reports.tsx
+++ frontend/components/ReportCharts/Reports.tsx
@@ -11,7 +11,7 @@ import {
 
 import React from "react";
 
-function Page() {
+function Reports() {
   return (
     <section className="section align-center mb-5">
       <h3 className="mb-10 text-3xl font-extrabold leading-none tracking-tight text-gray-700 md:text-3xl lg:text-4xl dark:text-white text-center">
@@ -31,4 +31,4 @@ function Page() {
   );
 }
 
-export default Page;
+export default Reports;
