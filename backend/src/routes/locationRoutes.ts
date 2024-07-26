e6cbe99e555d0654638855fb484f9a5237694054
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-26 20:24:28 +0200

    (update) Moved The Routes To Separate Directory

--- backend/src/routes/locationRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllLocations } from "../controllers/locationController";
-
-const router = express.Router();
-
-router.post("/", getAllLocations);
-
-export default router;
