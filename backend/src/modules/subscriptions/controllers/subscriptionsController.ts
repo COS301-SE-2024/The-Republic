b33946c2bb326c5d47c2f44067eabbe0e7e2cd79
tebogoyungmercykay <u20748052@tuks.co.za>
2024-08-03 21:13:05 +0200

    Backend Controller For Managing Subscriptions

---
+++ backend/src/modules/subscriptions/controllers/subscriptionsController.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from "express";
+import { sendResponse } from "@/utilities/response";
+import { APIResponse } from "@/types/response";
+import SubscriptionsService from "@/modules/subscriptions/services/subscriptionsService";
+
+const subscriptionsService = new SubscriptionsService();
+
+export const issueSubscriptions = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const response = await subscriptionsService.issueSubscriptions(
+      req.body,
+    );
+    sendResponse(res, response);
+  } catch (error) {
+    sendResponse(res, error as APIResponse);
+  }
+};
+
+export const categorySubscriptions = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const response =
+      await subscriptionsService.categorySubscriptions(req.body);
+    sendResponse(res, response);
+  } catch (error) {
+    sendResponse(res, error as APIResponse);
+  }
+};
+
+export const locationSubscriptions = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const response =
+      await subscriptionsService.locationSubscriptions(
+        req.body,
+      );
+    sendResponse(res, response);
+  } catch (error) {
+    sendResponse(res, error as APIResponse);
+  }
+};
+
+export const getSubscriptions = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const response = await subscriptionsService.getSubscriptions(req.body);
+    sendResponse(res, response);
+  } catch (error) {
+    sendResponse(res, error as APIResponse);
+  }
+};
