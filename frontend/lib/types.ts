5eb42e735737fe2c05bb6bc2a4bbd13c7921d859
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-08-03 12:41:42 +0200

    Updated types and constants

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -96,17 +96,22 @@ interface Issue {
     province: string;
     city: string;
     suburb: string;
+    latitude: string;
+    longitude: string;
   } | null;
   comment_count: number;
   is_owner: boolean;
   profile_user_id: string;
   user_reaction: string;
+  hasPendingResolution?: boolean;
+  pendingResolutionId?: string | null;
+  cluster_id?: string
 }
 interface IssueProps {
   issue: Issue;
   id?: string;
   onDeleteIssue?: (issue: Issue) => void;
-  onResolveIssue?: (issue: Issue, resolvedIssue: Issue) => void;
+  onResolveIssue?: (issue: Issue, resolution: Resolution) => void;
 }
 
 interface Comment {
@@ -279,6 +284,25 @@ interface MulterFile {
   buffer: Buffer;
 }
 
+interface Resolution {
+  resolution_id: string;
+  issue_id: number;
+  resolver_id: string;
+  resolution_text: string;
+  proof_image: string | null;
+  status: 'pending' | 'accepted' | 'declined';
+  created_at: string;
+  updated_at: string;
+  num_cluster_members: number;
+  num_cluster_members_accepted: number;
+  num_cluster_members_rejected: number;
+  political_association: string | null;
+  state_entity_association: string | null;
+  resolution_source: 'self' | 'unknown' | 'other';
+  resolved_by: string | null;
+}
+
+
 export type {
   AnalysisResult,
   FeedProps,
@@ -310,4 +334,5 @@ export type {
   MockUser,
   MulterFile,
   Location,
+  Resolution
 };
