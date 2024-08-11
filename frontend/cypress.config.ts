5a0c36469e2479443b192db07a6b804542266257
MulisaMusehane <127021696+MulisaMusehane@users.noreply.github.com>
2024-08-11 11:22:33 +0200

    Added backend url

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,8 +1,13 @@
+// cypress.config.ts
+
 import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    env: {
+      NEXT_PUBLIC_BACKEND_URL: "http://localhost:8080",
+    },
     setupNodeEvents() {
       // implement node event listeners here
     },
