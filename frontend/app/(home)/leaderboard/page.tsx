4648bd1d38c7b5708034c5223f88941ec0e7f54d
ShamaKamina <u18073655@tuks.co.za>
2024-08-28 11:38:10 +0200

    fixed leaderboard not appearing

---
+++ frontend/app/(home)/leaderboard/page.tsx
@@ -0,0 +1,7 @@
+import Leaderboard from '../../../components/Leaderboard/Leaderboard';
+
+const LeaderboardPage = () => {
+  return <Leaderboard />;
+};
+
+export default LeaderboardPage;
--- frontend/components/Leaderboard/Leaderboard.tsx
+++ frontend/components/Leaderboard/Leaderboard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect, useRef } from 'react';
 import { SlidersHorizontal } from 'lucide-react';
 import { useTheme } from 'next-themes';
