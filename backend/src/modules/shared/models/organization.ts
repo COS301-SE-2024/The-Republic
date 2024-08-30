72e4bd590ca0881dc8737fc4f6ab3a8792ae5a35
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-08-30 16:07:15 +0200

    Created all interface types

---
+++ backend/src/modules/shared/models/organization.ts
@@ -0,0 +1,28 @@
+export interface Organization {
+    id: string;
+    created_at: string;
+    name: string;
+    username: string;
+    bio: string | null;
+    website_url: string | null;
+    verified_status: boolean;
+    join_policy: string;
+    points: number;
+  }
+  
+  export interface OrganizationMember {
+    id: number;
+    organization_id: string;
+    user_id: string;
+    role: string;
+    joined_at: string;
+  }
+
+  export interface JoinRequest {
+    id: number;
+    organization_id: string;
+    user_id: string;
+    status: 'pending' | 'accepted' | 'rejected';
+    created_at: string;
+    updated_at: string;
+  }
