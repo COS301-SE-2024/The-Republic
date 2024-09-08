53db9bd85308e12ecf6db436168a27c8a864f5c8
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-09-08 15:57:02 +0200

    All imported components from shad with added semicolons

--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -30,7 +30,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  },
+  }
 );
 
 export interface ButtonProps
@@ -49,9 +49,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       />
     );
-  },
+  }
 );
-
 Button.displayName = "Button";
 
 export { Button, buttonVariants };
