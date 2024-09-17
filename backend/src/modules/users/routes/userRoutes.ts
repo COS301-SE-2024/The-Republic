c8bfc256c4299ed395c1e8fa9eccb4eb26c5c313
tebogoyungmercykay <u20748052@tuks.co.za>
2024-09-17 10:13:16 +0200

    Removed The VerifyUser Middleware from Exits Method

--- backend/src/modules/users/routes/userRoutes.ts
+++ backend/src/modules/users/routes/userRoutes.ts
@@ -4,27 +4,30 @@ import * as userController from "@/modules/users/controllers/userController";
 import { verifyAndGetUser } from "@/middleware/middleware";
 
 const router = Router();
-router.use(verifyAndGetUser);
 const upload = multer();
 
 router.post(
   "/:id",
+  verifyAndGetUser,
   userController.getUserById
 );
 
 router.put(
   "/:id",
+  verifyAndGetUser,
   upload.single("profile_picture"),
   userController.updateUserProfile
 );
 
 router.put(
   "/:id/username",
+  verifyAndGetUser,
   userController.updateUsername
 );
 
 router.put(
   "/:id/password",
+  verifyAndGetUser,
   userController.changePassword
 );
 
@@ -32,6 +35,5 @@ router.post(
   "/username/exists",
   userController.usernameExists
 );
-
 
 export default router;
--- frontend/lib/api/checkUsername.tsx
+++ frontend/lib/api/checkUsername.tsx
@@ -1,16 +1,11 @@
-import { UserAlt as User, UserExists } from "@/lib/types";
+import { UserExists } from "@/lib/types";
 
 export const checkUsername = async (
-  user: User,
   requestBody: UserExists,
 ) => {
   const headers: HeadersInit = {
     "Content-Type": "application/json",
   };
-
-  if (user) {
-    headers.Authorization = `Bearer ${user.access_token}`;
-  }
 
   const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/users/username/exists`;
   try {
