b7391fb6f36e605805c2429bb0a90954725b0d39
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-09-26 08:04:04 +0200

    Stop throwing errors endlessly when redis cannot connect

--- backend/src/modules/shared/services/redisClient.ts
+++ backend/src/modules/shared/services/redisClient.ts
@@ -7,13 +7,13 @@ try {
   redisClient = new Redis(process.env.REDIS_URL as string);
 
   redisClient.on("error", (err) => {
-    console.error("Redis Client Error", err);
+    // console.error("Redis Client Error", err);
     redisClient = null;
   });
 
   // redisClient.on('connect', () => console.log('Connected to Redis'));
 } catch (error) {
-  console.error("Failed to initialize Redis client:", error);
+  // console.error("Failed to initialize Redis client:", error);
 }
 
 export default redisClient;
