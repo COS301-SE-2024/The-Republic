0f7401f34b6eff48f483a8fb37030495b2aee8de
tebogoyungmercykay <u20748052@tuks.co.za>
2024-09-28 02:01:00 +0200

    Dark and Light Themes Mode for Charts

---
+++ frontend/lib/charts-dark-theme.tsx
@@ -0,0 +1,22 @@
+const darkTheme = {
+  textStyle: {
+    color: '#ffffff'
+  },
+  title: {
+    textStyle: {
+      color: '#ffffff'
+    }
+  },
+  legend: {
+    textStyle: {
+      color: '#ffffff'
+    }
+  },
+  tooltip: {
+    textStyle: {
+      color: '#ffffff'
+    }
+  }
+};
+
+export default darkTheme;
---
+++ frontend/lib/charts-light-theme.tsx
@@ -0,0 +1,22 @@
+const lightTheme = {
+  textStyle: {
+    color: '#333333'
+  },
+  title: {
+    textStyle: {
+      color: '#333333'
+    }
+  },
+  legend: {
+    textStyle: {
+      color: '#333333'
+    }
+  },
+  tooltip: {
+    textStyle: {
+      color: '#333333'
+    }
+  }
+};
+
+export default lightTheme;
