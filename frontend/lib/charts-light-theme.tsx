76b444035544183d947be48e8db9703f857f5db7
tebogoyungmercykay <u20748052@tuks.co.za>
2024-09-28 02:57:56 +0200

    Changed the Colors for the Light Theme

--- frontend/lib/charts-light-theme.tsx
+++ frontend/lib/charts-light-theme.tsx
@@ -1,22 +1,45 @@
 const lightTheme = {
   textStyle: {
-    color: '#333333'
+    color: '#141414'
   },
   title: {
     textStyle: {
-      color: '#333333'
+      color: '#141414'
     }
   },
   legend: {
     textStyle: {
-      color: '#333333'
+      color: '#141414'
     }
   },
   tooltip: {
+    backgroundColor: '#f1f5f0',
     textStyle: {
-      color: '#333333'
+      color: '#141414'
+    },
+    borderColor: '#afff9c',
+    borderWidth: 1
+  },
+  series: [{
+    type: 'pie',
+    itemStyle: {
+      borderColor: '#afff9c',
+      borderWidth: 1
+    },
+    label: {
+      color: '#141414'
+    },
+    emphasis: {
+      itemStyle: {
+        shadowBlur: 10,
+        shadowOffsetX: 0,
+        shadowColor: '#dadbd9'
+      },
+      label: {
+        color: '#141414'
+      }
     }
-  }
+  }]
 };
 
 export default lightTheme;
