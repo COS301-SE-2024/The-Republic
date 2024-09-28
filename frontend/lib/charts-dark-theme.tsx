4c3eb5a073925cac62925de735ce37ec1ba59ca6
tebogoyungmercykay <u20748052@tuks.co.za>
2024-09-28 02:56:52 +0200

    Update: Changed The Colors For Dark Theme

--- frontend/lib/charts-dark-theme.tsx
+++ frontend/lib/charts-dark-theme.tsx
@@ -13,10 +13,33 @@ const darkTheme = {
     }
   },
   tooltip: {
+    backgroundColor: 'rgba(50,50,50,0.8)',
     textStyle: {
       color: '#ffffff'
+    },
+    borderColor: '#555',
+    borderWidth: 1
+  },
+  series: [{
+    type: 'pie',
+    itemStyle: {
+      borderColor: '#1a1a1a',
+      borderWidth: 1
+    },
+    label: {
+      color: '#ffffff'
+    },
+    emphasis: {
+      itemStyle: {
+        shadowBlur: 10,
+        shadowOffsetX: 0,
+        shadowColor: 'rgba(255, 255, 255, 0.5)'
+      },
+      label: {
+        color: '#ffffff'
+      }
     }
-  }
+  }]
 };
 
 export default darkTheme;
