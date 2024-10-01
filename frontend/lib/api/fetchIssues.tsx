270e5b067e4a7c82288443bb47d9243156797caa
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-10-01 04:05:31 +0200

    fix

--- frontend/lib/api/fetchIssues.tsx
+++ frontend/lib/api/fetchIssues.tsx
@@ -31,9 +31,7 @@ const fetchIssues = async (
   }
 
   if (location) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { location_id, ...locationWithoutId } = location;
-    requestBody.location = locationWithoutId;
+    requestBody.location = location;
   }
 
   const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/issues`;
